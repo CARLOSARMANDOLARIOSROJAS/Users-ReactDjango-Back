74fbcf229c792b0b01b463c674d505fe76437787
CARLOSARMANDOLARIOSROJAS <20460440@colima.tecnm.mx>
2025-07-31 17:13:31 -0600

    Search field: username and email

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 # usuarios/views.py
 
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import User
 from .serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):   
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['username', 'email']
